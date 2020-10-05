83feb1220e6a9bad2d9defa4c0cf7e0bd1ab46bd
Sara Solera <sarasolera@correo.ugr.es>
2020-10-05 17:57:09 +0200

    Avance código, closes #4

---
+++ src/serie.js
@@ -0,0 +1,26 @@
+class Serie {
+    //constructor
+    constructor(){
+        var nombre;
+        var sinopsis;
+        var fecha_prox_temp;
+        var reparto = new Array();
+    
+    }
+
+    mostrarNombre(){
+        return nombre;
+    }
+
+    mostrarSinopsis(){
+        return sinopsis;
+    }
+
+    mostrarFechaP(){
+        return fecha_prox_temp;
+    }
+
+    mostrarReparto(){
+        
+    }
+}
