fac6280ae466acd4ca6997fa82c3a50ad0550ca0
Sara Solera <sarasolera@correo.ugr.es>
2020-11-04 17:26:39 +0100

    Añadiendo clase temporada, #38

---
+++ src/temporada.js
@@ -0,0 +1,38 @@
+var Capitulo = require("./capitulo")
+class Temporada{
+    constructor()
+    {
+        //Array de capitulos con su nombre sinopsis y duracion 
+        this.capitulos = new Array();
+    }
+
+    /**
+     * Captar los capitulos que tiene la temporada
+    */
+    getCapitulos(){
+       return this.capitulos;
+    }
+   /**
+    * Añadir capitulos de la temporada
+    * @param {array} capitulosTemporada
+    */
+    setCapitulos(capitulosTemporada){
+       this.capitulos = capitulosTemporada;
+    }
+
+    /**
+     * Añadir capitulo a la temporada
+     * @param {Capitulo} capitulo 
+     */
+    aniadirCapitulo(capitulo){
+       this.capitulos.push(capitulo);
+    }
+
+    eliminarCapitulo(index){
+        if(index > this.capitulos.length){
+            this.capitulos.pop(index);
+        }
+        else
+            throw new Error("Indice inválido");
+    }
+}
