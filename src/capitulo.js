9ff780b9628c59fa7ff63984329d6c4b03c3f585
Sara Solera <sarasolera@correo.ugr.es>
2020-11-04 17:23:31 +0100

    Añado clase capitulo, #38

---
+++ src/capitulo.js
@@ -0,0 +1,58 @@
+class Capitulo{
+    constructor(nombre,duracion,sinopsis){
+        this.nombre = nombre
+        this.duracion = duracion
+        this.sinopsis = sinopsis
+    }
+    /**
+     * Obtener el nombre del capitulo
+     * @return nombre del Capitulo
+     */
+    getNombre()
+    {
+        return this.nombre;
+    }
+    /**
+     * Obtener la duración del capitulo
+     * @return duracion del Capitulo
+     */
+    getDuracion(){
+        return this.duracion;
+    }
+
+    /**
+     * Obtener la sinopsis del capitulo
+     * @return sinopsis del capitulos
+     */
+
+    getSinopsis(){
+        return this.sinopsis;
+    }
+
+    /** Métodos de modificación de los datos */
+    /**
+     * Modificación nombre del capitulo
+     * @param {string} nombreNuevo 
+     */
+    setNombre(nombreNuevo){
+        this.nombre = nombreNuevo;
+    }
+    /**
+     * Modificación duracion de capitulo en minutos 
+     * @param {int} duracionNueva 
+     */
+    setDuracion(duracionNueva)
+    {
+        this.duracion = duracion;
+    }
+    
+    /**
+     * Modificación de la sinopsis del capitulo
+     * @param {string} sinopsisNueva
+     */
+    setSinopsis(sinopsisNueva){
+        this.sinopsis = sinopsisNueva;
+    }
+
+
+}
