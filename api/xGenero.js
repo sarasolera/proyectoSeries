882fa91ea5c182aad512ffa96e4ee8968d0ba291
sarasolera <sarasolera@correo.ugr.es>
2020-11-24 10:43:07 +0100

    Comprobando datos obtenidos en el req, #43

--- api/xGenero.js
+++ api/xGenero.js
@@ -1,5 +1,13 @@
 module.exports = async (req,res) =>{
     //Que contiene req.body
-    result=JSON.stringify(req.body)
-    res.status(200).send(result)
+    var IDchat = req.body.message.chat.id
+    var text = req.body.message.text
+
+    if(text == "/muestraBody"){
+        var result = JSON.stringify(req.body)
+    }
+
+    var objetoJSON ={text : result,method : "sendMessage",chat_id : id_chat}
+    res.setHeader("Content-Type","application/json");
+    res.status(200).json(objetoJSON)
 }
