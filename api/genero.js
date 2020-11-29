2c17af315b508dfb88738755bcb9e21ec4c98b9a
sarasolera <sarasolera@correo.ugr.es>
2020-11-29 19:31:27 +0100

    Arreglando formato cuando hay error, #42 #43 [skip ci]

--- api/genero.js
+++ api/genero.js
@@ -12,11 +12,13 @@ module.exports = (req,res) =>{
     }
     //Si es Vacio significa que no se ha recibido nada
     else if(genero == "Vacio"){
-        result="Error: debe indicar un género";
+        result={"Error 400":"debe indicar un género"} 
         codigo = 400
+    
+           
     }
     else{
-        result="Error: género no disponible. Los géneros disponibles por el momento son: MIEDO ACCION DRAMA COMEDIA";
+        result={"Error 404" : "género no disponible. Los géneros disponibles por el momento son: MIEDO ACCION DRAMA COMEDIA"};
         codigo = 404
 
     }
