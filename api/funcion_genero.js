65bd30dc7fda4fad6d4470925096cca553d457ec
sarasolera <sarasolera@correo.ugr.es>
2020-11-29 18:44:30 +0100

    Implementando una función general para no repetir código, #42 #43 [skip ci]

---
+++ api/funcion_genero.js
@@ -0,0 +1,31 @@
+const data = require("./data");
+const Serie  = require("../src/serie");
+const Catalogo = require("../src/catalogo");
+
+//AÑADIMOS FUNCION PARA CAPTAR SERIES POR GENERO
+function devolverSeriesGenero(genCaptado){
+    //Primero formamos el catalogo de series con data
+    var i = 0;
+    var catalogo = new Catalogo();
+    while( i < data.data.length){
+        nombre= data.data[i]['Nombre'];
+        sinopsis= data.data[i]["Sinopsis"];
+  
+        num_temporadas = data.data[i]["Numero Temporadas"];
+        gen = data.data[i]["Genero"];
+        i+=1
+        var serie = new Serie(nombre,sinopsis,num_temporadas,gen)
+        catalogo.aniadirSerie(serie)
+
+    }
+    var result = ""
+    //Tras ello captamos la series por genero
+    seriesGenero = catalogo.mostrarSeriesGenero(genCaptado)
+    for(var j = 0; j < seriesGenero.length ; j++){
+        result += j + ":" + seriesGenero[j].getNombre() + "\n"
+    }
+
+    return result
+}
+
+module.exports.devolverSeriesGenero = devolverSeriesGenero;
--- api/genero.js
+++ api/genero.js
@@ -1,60 +1,28 @@
-const data = require("./data");
-const Serie  = require("../src/serie");
-const Catalogo = require("../src/catalogo");
+const funcion = require("./funcion_genero.js")
 
 module.exports = (req,res) =>{
     //Captaremos de la URL el genero del que queremos consultar las series
     //Si no obtenemos ningun por defecto valor "Vacio" para generar el resultado correspondiente
 
     const{genero="Vacio"} = req.query
-    var result;
-    var catalogo = new Catalogo();
-    var i = 0;
 
-    //Generamos las series captando del fichero data.js para añadirlas al catalogo
-    while( i < data.data.length){
-        nombre= data.data[i]['Nombre'];
-        sinopsis= data.data[i]["Sinopsis"];
-  
-        num_temporadas = data.data[i]["Numero Temporadas"];
-        gen = data.data[i]["Genero"];
-        i+=1
-        var serie = new Serie(nombre,sinopsis,num_temporadas,gen)
-        catalogo.aniadirSerie(serie)
-
-    }
-
-    var datosJSON = []
-    var objetoJSON = {}
-    // Una vez generado el catalogo podemos captar las series por género, con la función que tenemos implementada
-    // Voy a meter las series en una lista de diccionarios para pasarlos a json
     // Si obtenemos un genero en la petición captamos las series de ese género
     if(genero == "MIEDO" || genero=="COMEDIA" || genero=="ACCION" || genero=="DRAMA"){
-        var seriesGenero = catalogo.mostrarSeriesGenero(genero)
-        
-        seriesGenero.forEach(element => {
-            datosJSON.push({
-                "Nombre serie ": element.getNombre(),
-                "Sinopsis": element.getSinopsis(),
-                "Número Temporadas":element.getNumTemporadas()
-            });
-            
-        });
-        // FORMAMOS EL JSON
-        objetoJSON.series = datosJSON;
-        result = JSON.stringify(objetoJSON)
-        
+        result = funcion.devolverSeriesGenero(genero)
+        codigo = 200
     }
     //Si es Vacio significa que no se ha recibido nada
     else if(genero == "Vacio"){
         result="Debe indicar un género";
+        codigo = 400
     }
     else{
         result="Género no disponible. Los géneros disponibles por el momento son: MIEDO ACCION DRAMA COMEDIA";
+        codigo = 404
 
     }
 
-    // ENVIAMOS el resultado
-    res.status(200).send(result)
+    // ENVIAMOS el resultado n
+    res.status(codigo).send(result)
 
 }
