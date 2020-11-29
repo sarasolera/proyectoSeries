d6ec3f85c33148cc82182e4a9fd353f9c65b0ff2
sarasolera <sarasolera@correo.ugr.es>
2020-11-29 20:18:17 +0100

    Corrigiendo resultado de la función, #45 [skip ci]

--- api/xGenero.js
+++ api/xGenero.js
@@ -8,44 +8,28 @@ module.exports = async (req,res) =>{
         //Captamos contenido de mensaje
         var text = req.body.message.text
         var result = ""
+        var series_captadas = false
         // Según el contenido obtenemos una lista de series u otra
         if(text == "/xGeneroMIEDO"){
             result="Buscando series de Miedo....\n";
-            series_captadas=funcion.devolverSeriesGenero("MIEDO")
-            j = 0
-            series_captadas.series.forEach(element => {
-                result+= j + ":" + element + "\n"; 
-                j+=1
-            });
-            
+            series_captadas = true;
+            captadas=funcion.devolverSeriesGenero("MIEDO")
         }
         else if(text == "/xGeneroCOMEDIA"){
             result = "Buscando series de Comedia...\n"
-            series_captadas = funcion.devolverSeriesGenero("COMEDIA")
-            j = 0
-            series_captadas.series.forEach(element => {
-                result+= j + ":" + element + "\n"; 
-                j+=1
-            });
-            
+            series_captadas = true;
+            captadas = funcion.devolverSeriesGenero("COMEDIA")
         }
         else if(text == "/xGeneroDRAMA"){
             result="Buscando series de Drama... \n"
-            series_captadas=funcion.devolverSeriesGenero("DRAMA")
-            j = 0
-            series_captadas.series.forEach(element => {
-                result+= j + ":" + element + "\n"; 
-                j+=1
-            });
+            series_captadas = true;
+            captadas=funcion.devolverSeriesGenero("DRAMA")
         }
         else if(text == "/xGeneroACCION"){
             result="Buscando series de Acción...\n"
-            series_captadas=funcion.devolverSeriesGenero("ACCION")
-            j = 0
-            series_captadas.series.forEach(element => {
-                result+= j + ":" + element + "\n"; 
-                j+=1
-            });
+            series_captadas = true;
+            captadas=funcion.devolverSeriesGenero("ACCION")
+            
         }
         else if(text == "/muestraBody"){
             result+=JSON.stringify(req.body)
@@ -54,7 +38,11 @@ module.exports = async (req,res) =>{
             result="Para consultar las series por cada genero tiene disponibles:\n/xGeneroMIEDO\n/xGeneroACCION\n/xGeneroCOMEDIA\n/xGeneroDRAMA"
         }
 
-    
+        if(series_captadas == true){
+            for(var j = 0; j< captadas.series.length ; j++){
+                result+= j + ":" + captadas.series[i] + "\n";
+            }
+        }
         // Devolvemos 
         var objetoJSON ={text : result,method : "sendMessage",chat_id : IDchat}
         res.setHeader("Content-Type","application/json");
