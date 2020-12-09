2a323b89b3a41ef1a866d3cbf4be9fbb8cde04dd
sarasolera <sarasolera@correo.ugr.es>
2020-12-09 02:17:26 +0100

    Añado el complemento que me servira como manejador de errores, #48 #47 [skip ci]

--- src/rutas/rutas.js
+++ src/rutas/rutas.js
@@ -5,7 +5,29 @@ const controlador_s = new Controlador();
 module.exports = {
     name: 'ApiRutas',
     register: async(server,options) => {
-       
+        //generamos el complemento que sera lanzado antes de enviar la respuesta
+        server.ext('onPreResponse', function (request, reply){
+            var res = request.response;
+            var req = request.raw.req;
+            //Si el objeto request.response es tipo boom significa que ocurrió un error
+            //Captamos el mensaje y el codigo (generados a con mis clase error)
+            if(res.isBoom){
+                console.log(res)
+                if(res.mensaje){
+                    //si existe mensaje, es que la excepcion fue mandada a traves de mi clase error asi que captamos mensaje y codigo
+                    return reply.response(res.mensaje).code(res.codigo);
+                }
+                else{
+                    //si no podemos coger del cuerpo que proporciona res
+                    return reply.response(res.output.payload.message).code(res.output.statusCode);
+                }
+                
+            }
+            //si no es boom podemos continuar
+            else{
+                return reply.continue;
+            }
+        });
         server.route([
             {
                 method: 'GET',
