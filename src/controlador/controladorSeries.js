c5fb6ac2e35ab6f7641bcd08f4e4eadcf7c6eacf
sarasolera <sarasolera@correo.ugr.es>
2020-12-10 16:53:29 +0100

    Añadiendo controlador series, #47 [skip ci]

--- src/controlador/controladorSeries.js
+++ src/controlador/controladorSeries.js
@@ -8,12 +8,16 @@ class Controlador{
     }
     
     obtenerSeries(){
+        // captamos todas las series del catalogo
         var catCompleto = [];
         catCompleto = this.catalogo.mostrarSeries();
         var lista = [];
+        //Despues de cada \n tenemos el nombre de una serie, por lo que filtramos y el ultimo elemento será vacio
+        //asi que lo eliminamos
         lista = catCompleto.split("\n");
         lista.pop();
         var listaDatos = []
+        //generamos una lista Nombre: nombre_serie
         lista.forEach(element => {
         listaDatos.push({"Nombre":element});
         });
