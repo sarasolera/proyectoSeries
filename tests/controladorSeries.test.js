bcb71fea728c5ae620aaaf341dfbabfdec6bfe23
sarasolera <sarasolera@correo.ugr.es>
2020-12-10 18:18:04 +0100

    Testeando función que permite obtener datos de una serie según su nombre, HU01 #7 #49 #47

--- tests/controladorSeries.test.js
+++ tests/controladorSeries.test.js
@@ -1,4 +1,5 @@
 const Controlador = require('../src/controlador/controladorSeries');
+const ErrorPropio = require('../src/error');
 
 const controlador = new Controlador();
 
@@ -14,4 +15,19 @@ describe("Testeando clase Controlador", ()=>{
         var num_series = controlador.catalogo.series.length
         expect(lista_Series.length).toBe(num_series);
     });
+
+    test("Metodo para obtener una serie del catalogo, dando su nombre",()=>{
+        //Obtenemos los datos de la serie Barry.
+        const n_serie = "Barry"
+        var listaDatos = controlador.obtenerSerie(n_serie);
+        var dic = {"Nombre":controlador.catalogo.series[2].getNombre(),"Sinopsis":controlador.catalogo.series[2].getSinopsis()};
+        const listaEsperada = [];
+        listaEsperada.push(dic);
+        expect(listaDatos).toStrictEqual(listaEsperada);
+        var errorObtenido = () => controlador.obtenerSerie("NoExiste")
+        var errorEsperado =  new ErrorPropio("Esa serie no existe",404);
+        expect(errorObtenido).toThrow(errorEsperado);
+        
+
+    })
 })
