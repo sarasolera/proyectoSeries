fd99edc84cfffa132f1d9121e2e5919815da1b3e
sarasolera <sarasolera@correo.ugr.es>
2020-12-12 09:56:07 +0100

    Añadiendo tareas [skip ci]

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         //vamos a ejecutar
         run:{
-            // los test, para ello:
+            //los test, para ello:
             test:{
                 //ejecutamos npm 
                 cmd: 'npm',
@@ -13,11 +13,34 @@ module.exports = function(grunt) {
                     'test'
                 ]
             }
+        },
+
+        uglify:{
+            build:{
+                cmd:'npm',
+                args:[
+                    'build'
+                ]
+            }
         }
+        
     });
 
     //Carga el plugin de grunt para hacer la tarea
     grunt.loadNpmTasks('grunt-run');
+    //cargamos el plugin para hacer el install
+    grunt.loadNpmTasks('grunt-npm-install');
+
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+   
+    //
+    grunt.registerTask('build',["uglify"]);
     //Tarea por omisión: generar la documentación
     grunt.registerTask('test',["run"]);
+
+    //registramos la tarea
+    grunt.registerTask('install',["npm-install"]);
+    
+    //registramos la tarea
+   
 };
